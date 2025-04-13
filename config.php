8b783217768566b370bb84b621ae3439830be84a
GosTnT <goztnt@gmail.com>
2025-04-13 07:24:25 -0300

    aaa
    aaaaa

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection (using PDO)
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "register";
+$servername = "mysql://fulls_lawmaybehe:bf66f0e48de96fca850685dbccfbc2c96d6c58a8@d5c0v.h.filess.io:61001/fulls_lawmaybehe";
+$username = "fulls_lawmaybehe";
+$password = "bf66f0e48de96fca850685dbccfbc2c96d6c58a8";
+$dbname = "fulls_lawmaybehe";
 
 try {
     // Create a new PDO instance
